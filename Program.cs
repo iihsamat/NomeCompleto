db659792a75f7a08df5df15665dcaadf3b875dc0
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2023-08-25 20:12:31 -0300

    trabalho nome completo

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿//Stefeson
+Console.Clear();
+
+Console.WriteLine("Qual seu nome?");
+string nome = Console.ReadLine();
+
+Console.WriteLine("Qual seu sobrenome?");
+string sobrenome = Console.ReadLine();
+
+Console.Clear();
+
+Console.WriteLine($"Seu nome completo é: {nome} {sobrenome}");
+
+Thread.Sleep(3000);
+Console.WriteLine($"Seu nome de catálogo é: {sobrenome}, {nome}.");
